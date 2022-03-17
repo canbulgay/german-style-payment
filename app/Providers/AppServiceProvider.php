6190f01e382db571156ffb2920710e4abf8b032f
canbulgay <canbulgay@outlook.com>
2022-03-17 23:51:39 +0300

    Create dashboard page for available tables to choose, register checks instance  at appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Check;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        try{
+            View::share('checks',Check::all());
+        }catch(\Throwable $th){
+            View::share('checks',[]);
+        }
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,24 @@
-<h1>
-    Burası Dashboard!
-    @if (session('success'))
-        <div class="alert alert-success">{{session('success')}}</div>
-    @endif
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <div class="container mx-auto p-48">
+        <h1 class="text-indigo-400 text-2xl text-center bold">You must choose your table before seeing the bill </h1>
+        <div class="flex justify-center space-x-10">
+            @foreach ($checks as $check)
+            <div class="bg-indigo-600 text-white rounded-full w-24 h-24 relative grid place-items-center mt-8">
+                <div class="absolute">
+                    <a  href="/dashboard/{{$check->id}}"><span class="text-center">Table {{$check->id}}</span></a>
+                </div>
+            </div>
+            @endforeach 
+        </div>
+    </div>
+</body>
+</html>
