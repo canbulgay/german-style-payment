d48193b5e8401b0a9003d15d480671f5a97f055b
canbulgay <canbulgay@outlook.com>
2022-03-21 23:01:06 +0300

    Create name and total attribute getters in ItemOrder model

--- app/Models/ItemOrder.php
+++ app/Models/ItemOrder.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class ItemOrder extends Pivot
 {
-    use HasFactory;
+    public function getNameAttribute()
+    {
+        $itemId = $this->item_id;
+        $item = Item::find($itemId);
+        $itemName = $item->name;
+
+        return $itemName;
+    }
+    public function getTotalAttribute()
+    {
+        $total = $this->quentity * $this->rate;
+        return $total;
+    }
 }
